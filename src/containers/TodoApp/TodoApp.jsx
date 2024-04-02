06f0f8f440af75195112755d3d6839810e4706c9
Aldo Jure <dev.aldojure@gmail.com>
2024-04-02 12:44:35 -0300

    Se crea la estructura principal de la aplicación

--- src/containers/TodoApp/TodoApp.jsx
+++ src/containers/TodoApp/TodoApp.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import Input from '../../components/Input/Input'
+import Button from '../../components/Button/Button'
 
 function TodoApp() {
-    
+  return (
+    <section>
+      <div>
+        <h1>Ingrese su tarea:</h1>
+        <Input />
+        <Button />
+      </div>
+      <div>
+        <h3>Tareas:</h3>
+        <div>
+          <Button />
+        </div>
+      </div>
+      <div>
+        <h3>Completadas:</h3>
+      </div>
+    </section>
+  )
 }
 
 export default TodoApp

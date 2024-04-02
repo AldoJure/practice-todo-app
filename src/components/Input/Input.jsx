60926898ab293fdade8c5059d6382d9d65d12820
Aldo Jure <dev.aldojure@gmail.com>
2024-04-02 13:02:02 -0300

    Se optimizo y creo funcionalidad para agregar tareas a la lista de tareas

--- src/components/Input/Input.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-function Input() {
-
-}
-
-export default Input
--- src/containers/TodoApp/TodoApp.jsx
+++ src/containers/TodoApp/TodoApp.jsx
@@ -1,19 +1,33 @@
-import React from 'react'
-import Input from '../../components/Input/Input'
-import Button from '../../components/Button/Button'
+import React, { useState } from 'react'
 
 function TodoApp() {
+  const [task, setTask] = useState(null)
+  const [taskList, setTaskList] = useState([])
+
+  function handleChange(event) {
+    setTask(event.target.value)
+  }
+
+  function handleClick() {
+    setTaskList(prevTasks => {
+      return [...prevTasks, task]
+    })
+  }
   return (
     <section>
       <div>
         <h1>Ingrese su tarea:</h1>
-        <Input />
-        <Button />
+        <input 
+        placeholder='Aprender React.js'
+        onChange={handleChange} />
+        <button onClick={handleClick}>Guardar</button>
       </div>
       <div>
         <h3>Tareas:</h3>
         <div>
-          <Button />
+          <input type='checkbox' />
+          <p>{taskList}</p>
+          <button>Borrar</button>
         </div>
       </div>
       <div>

2318354d5e6938812fc3e8c25179afa6106f9582
Aldo Jure <dev.aldojure@gmail.com>
2024-04-02 17:24:00 -0300

    Se agrega funcionalidad para pasar a completado y quitar de lista de tareas

--- src/containers/TodoApp/TodoApp.jsx
+++ src/containers/TodoApp/TodoApp.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 function TodoApp() {
   const [task, setTask] = useState(null)
   const [taskList, setTaskList] = useState([])
+  const [completedTasks, setCompletedTasks] = useState([])
 
   function handleChange(event) {
     setTask(event.target.value)
@@ -13,25 +14,57 @@ function TodoApp() {
       return [...prevTasks, task]
     })
   }
+
+  function handleTask(event) {
+    const { target } = event
+    const index = target.getAttribute('dataindex')
+    if(target.type === 'checkbox'){
+      //console.log(`El evento se ejecuto desde un checkbox y la tarea es: ${taskList[index]}`);
+      setCompletedTasks(prevCompletedTasks => {
+        return [...prevCompletedTasks, taskList[index]]
+      })
+      setTaskList(taskList.filter(task => task !== taskList[index]))
+    }
+    if(event.target.value === 'Borrar'){
+      setTaskList(prevTasks => {
+        
+      })
+    }
+  }
   return (
     <section>
       <div>
         <h1>Ingrese su tarea:</h1>
-        <input 
-        placeholder='Aprender React.js'
-        onChange={handleChange} />
+        <input
+          placeholder='Aprender React.js'
+          onChange={handleChange} />
         <button onClick={handleClick}>Guardar</button>
       </div>
       <div>
         <h3>Tareas:</h3>
         <div>
-          <input type='checkbox' />
-          <p>{taskList}</p>
-          <button>Borrar</button>
+          {taskList.map((task, index) => {
+            return (
+              <>
+                <input 
+                dataindex={index}
+                type='checkbox' 
+                onChange={handleTask} 
+                checked={false}
+                />
+                <p>{task}</p>
+                <button 
+                dataindex={index} 
+                onChange={handleTask}>Borrar</button>
+              </>
+            )
+          })
+          }
         </div>
       </div>
       <div>
         <h3>Completadas:</h3>
+        <p>{completedTasks}</p>
       </div>
     </section>
   )
